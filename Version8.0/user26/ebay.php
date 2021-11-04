c81e95c6abeb7d0af88b38b5366c2dd15085944a
Evan You <226745@shakopeeschools.org>
2021-11-04 08:41:35 -0500

    version 8
    worked on weather api and ebay api

--- Version8.0/user26/ebay.php
+++ Version8.0/user26/ebay.php
@@ -6,44 +6,47 @@ $endpoint = 'http://svcs.ebay.com/services/search/FindingService/v1';  // URL to
 $version = '1.0.0';  // API version supported by your application
 $appid = 'RobertMa-Shakopee-PRD-169ec6b8e-bb30ba02';  // Replace with your own AppID
 $globalid = 'EBAY-US';  // Global ID of the eBay site you want to search (e.g., EBAY-DE)
-$query = 'legoes';  // You may want to supply your own query
+$query = 'screwdrivers';  // You may want to supply your own query
 $safequery = urlencode($query);  // Make the query URL-friendly
 $i = '0';  // Initialize the item filter index to 0
 // Create a PHP array of the item filters you want to use in your request
 $filterarray =
   array(
     array(
-    'name' => 'MaxPrice',
-    'value' => '300',
-    'paramName' => 'Currency',
-    'paramValue' => 'USD'),
+      'name' => 'MaxPrice',
+      'value' => '25',
+      'paramName' => 'Currency',
+      'paramValue' => 'USD'
+    ),
     array(
-    'name' => '',
-    'value' => 'true',
-    'paramName' => '',
-    'paramValue' => ''),
+      'name' => '',
+      'value' => 'true',
+      'paramName' => '',
+      'paramValue' => ''
+    ),
     array(
-    'name' => 'ListingType',
-    'value' => array('AuctionWithBIN','FixedPrice'),
-    'paramName' => '',
-    'paramValue' => ''),
+      'name' => 'ListingType',
+      'value' => array('AuctionWithBIN', 'FixedPrice'),
+      'paramName' => '',
+      'paramValue' => ''
+    ),
   );
 
 // Generates an indexed URL snippet from the array of item filters
-function buildURLArray ($filterarray) {
+function buildURLArray($filterarray)
+{
   global $urlfilter;
   global $i;
   // Iterate through each filter in the array
-  foreach($filterarray as $itemfilter) {
+  foreach ($filterarray as $itemfilter) {
     // Iterate through each key in the filter
-    foreach ($itemfilter as $key =>$value) {
-      if(is_array($value)) {
-        foreach($value as $j => $content) { // Index the key for each value
+    foreach ($itemfilter as $key => $value) {
+      if (is_array($value)) {
+        foreach ($value as $j => $content) { // Index the key for each value
           $urlfilter .= "&itemFilter($i).$key($j)=$content";
         }
-      }
-      else {
-        if($value != "") {
+      } else {
+        if ($value != "") {
           $urlfilter .= "&itemFilter($i).$key=$value";
         }
       }
@@ -73,14 +76,14 @@ $resp = simplexml_load_file($apicall);
 if ($resp->ack == "Success") {
   $results = '';
   // If the response was loaded, parse it and build links
-  foreach($resp->searchResult->item as $item) {
+  foreach ($resp->searchResult->item as $item) {
     $pic   = $item->galleryURL;
     $link  = $item->viewItemURL;
     $title = $item->title;
-/////////////////////////EDIT THIS LINE/////////////////////////////////////////////////////
+    /////////////////////////EDIT THIS LINE/////////////////////////////////////////////////////
     // For each SearchResultItem node, build a link and append it to $results
-    $results .= "<div class='col'><td><img src=\"$pic\"></td><td><a href=\"$link\">$title</a></td></div>";
-////////////////////////EDIT THIS LINE//////////////////////////////////////////////////////      
+    $results .= "<tr><td><img src=\"$pic\"></td><td><a href=\"$link\">$title</a></td></tr>";
+    ////////////////////////EDIT THIS LINE//////////////////////////////////////////////////////      
   }
 }
 // If the response does not indicate 'Success,' print an error
@@ -93,30 +96,41 @@ else {
 <html>
 
 <head>
-    <!-- Bootstrap meta data -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>eBay Search Results for <?php echo $query; ?></title>
 
-    <title>eBay Search Results for <?php echo $query; ?></title>
-    <style type="text/css">
-        body {
-            font-family: arial, sans-serif;
-        }
+  <!-- CSS -->
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- Animate -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+  <!-- Custom -->
+  <link rel="stylesheet" href="style.css">
 
-    </style>
+  <!-- JavaScript -->
+  <!-- These are needed to get the responsive menu to work -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <style type="text/css">
+    body {
+      font-family: arial, sans-serif;
+    }
+  </style>
 </head>
 
 <body>
 
-    <h1>eBay Search Results for <?php echo $query; ?></h1>
+  <h1>eBay Search Results for <?php echo $query; ?></h1>
 
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col">
-                <?php echo $results;?>
-        </div>
-    </div>
+  <table>
+    <tr>
+      <td>
+        <?php echo $results; ?>
+      </td>
+    </tr>
+  </table>
 
 </body>
 
